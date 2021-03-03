3536d06cb5e6f2bd0018d632fe80c91c13f93a8f
kayamayoki <k32142004@gmail.com>
2021-03-03 14:49:28 +0000

    応用課題３

---
+++ bookers2-comment/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  get 'home/about' => 'homes#about'
+end
