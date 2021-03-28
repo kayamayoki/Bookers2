6ee3fc460b647b87f30de2e2da1cded183d74271
kayamayoki <k32142004@gmail.com>
2021-03-28 13:51:02 +0000

    juusyo

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,43 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+(function() {
+  (document).on('turbolinks:load', () => {
+    ('#user_postal_code').jpostal({
+      postcode : [
+        '#user_postal_code'
+      ],
+      address: {
+        "#user_prefecture_code": "%3", // # 都道府県が入力される
+        "#user_city"           : "%4%5", // # 市区町村と町域が入力される
+        "#user_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+      }
+    });
+  });
+});
+
+
+// # 入力項目フォーマット
+// #   %3  都道府県
+// #   %4  市区町村
+// #   %5  町域
+// #   %6  大口事業所の番地 ex)100-6080
+// #   %7  大口事業所の名称
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+
+  root 'homes#top'
+  get 'mypage', to: 'homes#mypage'
+
+end
